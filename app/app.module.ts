99f15bb555a13ec6d45c5296c7780b237ab1d538
Maria Rosado <mrosado2@cnm.edu>
2016-11-07 17:01:23 -0700

    added quickstart

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,7 +4,6 @@ import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
 import {AppComponent} from "./app.component";
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
-import {DicewareService} from "./services/diceware-service";
 
 const moduleDeclarations = [AppComponent];
 
